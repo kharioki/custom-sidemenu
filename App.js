65cd877d7ae274bb41879a65724673b41daaaa51
kharioki <tony.kharioki@gmail.com>
2023-01-09 01:01:20 +0300

    animated overlay

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react'
-import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useRef, useState } from 'react'
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Animated } from 'react-native';
 import profile from './assets/profile.png';
 // Tab Icons ...
 import home from './assets/home.png';
@@ -8,9 +8,21 @@ import search from './assets/search.png';
 import notifications from './assets/bell.png';
 import settings from './assets/settings.png';
 import logout from './assets/logout.png';
+// Menu icons ...
+import menu from './assets/menu.png';
+import close from './assets/close.png';
+// Photo
+import photo from './assets/photo.jpg';
 
 export default function App() {
   const [currentTab, setCurrentTab] = useState('Home');
+  const [showMenu, setShowMenu] = useState(false);
+
+  // Animated properties...
+  const offsetValue = useRef(new Animated.Value(0)).current;
+  // Scale initially must be 1
+  const scaleValue = useRef(new Animated.Value(1)).current;
+  const closeButtonOffset = useRef(new Animated.Value(0)).current;
 
   return (
     <SafeAreaView style={styles.container}>
@@ -50,6 +62,73 @@ export default function App() {
         </View>
 
       </View>
+
+      {/* Overlay view */}
+      <Animated.View style={{
+        flexGrow: 1,
+        backgroundColor: '#fff',
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        right: 0,
+        left: 0,
+        paddingHorizontal: 15,
+        paddingVertical: 20,
+        // Transform the view...
+        transform: [
+          { scale: scaleValue },
+        ],
+      }}>
+
+        {/* Menu Button */}
+        <TouchableOpacity onPress={() => {
+          // Do Actions Here...
+          // Scaling the view...
+          Animated.timing(scaleValue, {
+            toValue: showMenu ? 1 : 0.8,
+            duration: 300,
+            useNativeDriver: true,
+          }).start();
+
+          setShowMenu(!showMenu);
+        }}>
+
+          <Image source={menu} style={{
+            width: 20,
+            height: 20,
+            tintColor: 'black',
+            marginTop: 30,
+          }} />
+
+        </TouchableOpacity>
+
+        <Text style={{
+          fontSize: 30,
+          fontWeight: 'bold',
+          color: 'black',
+          paddingTop: 20,
+        }}>{currentTab}</Text>
+
+        <Image source={photo} style={{
+          width: '100%',
+          height: 300,
+          borderRadius: 15,
+          marginTop: 20,
+        }} />
+
+        <Text style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          paddingTop: 15,
+          paddingBottom: 8,
+        }}>Tony Stark</Text>
+
+        <Text style={{}}>
+          Techie, Woodworker, and a Marvel Fanatic. I love to build shit!!!
+        </Text>
+
+
+      </Animated.View>
     </SafeAreaView>
   );
 }
